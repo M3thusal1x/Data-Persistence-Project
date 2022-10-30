94ac280dac7c1afc5026bdf9fb9fdbb241b0bf51
M3thusal1x <117019949+M3thusal1x@users.noreply.github.com>
2022-10-30 19:33:34 +0100

    Persistence between Sessions
    - SaveData class
    - Methods Save and Load

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance;
 
     public string PlayerName;
+    public string HighScoreName;
+    public int HighScore;
 
     // Implemented as singleton
     private void Awake()
@@ -21,9 +24,38 @@ public class DataManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class SaveData
     {
-        
+        public string LastPlayer;
+        public string HighScoreName;
+        public int HighScore;
+    }
+
+    public void Save()
+    {
+        SaveData data = new SaveData();
+        data.LastPlayer = PlayerName;
+        data.HighScoreName = HighScoreName;
+        data.HighScore = HighScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void Load()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            PlayerName = data.LastPlayer;
+            HighScoreName = data.HighScoreName;
+            HighScore = data.HighScore;
+        }
     }
 }
